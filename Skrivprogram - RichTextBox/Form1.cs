f865ad69eae8c55183146f46ece9e692bc6ca6d8
Emil Zackrisson <emil.zackrisson@gmail.com>
2023-01-17 16:25:28 +0100

    Can save and open file with font and size

--- Skrivprogram - RichTextBox/Form1.cs
+++ Skrivprogram - RichTextBox/Form1.cs
@@ -148,6 +148,7 @@ namespace Skrivprogram___RichTextBox
 
         private void saveAs()
         {
+            /*
             // Sparar en fil med ett nytt fränt filformat (.skitformat)
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
@@ -158,10 +159,24 @@ namespace Skrivprogram___RichTextBox
                 string filenameWithExtention = Path.GetFileName(saveFileDialog1.FileName);
                 Form1.ActiveForm.Text = filenameWithExtention;
             }
+            */
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                savePath = saveFileDialog1.FileName;
+
+                string text = läsFormattering();
+                File.WriteAllText(savePath, text);
+
+                sparat = true;
+                string filenameWithExtention = Path.GetFileName(saveFileDialog1.FileName);
+                Form1.ActiveForm.Text = filenameWithExtention;
+            }
         }
 
-        private void save()
+        private void save(string text)
         {
+            /*
             // Sparar en fil med ett nytt fränt filformat (.skitformat)
             try
             {
@@ -181,11 +196,14 @@ namespace Skrivprogram___RichTextBox
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error while saving", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            } */
+
+
         }
 
         private void open()
         {
+            /*
             // Öppnar en fil med ett nytt fränt filformat (.skitformat)
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
@@ -193,6 +211,12 @@ namespace Skrivprogram___RichTextBox
                 sparat = true;
                 Form1.ActiveForm.Text = openFileDialog1.SafeFileName;
                 savePath = openFileDialog1.FileName;
+            }*/
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string text = File.ReadAllText(openFileDialog1.FileName);
+                decodeFormatting(text);
             }
         }
 
@@ -201,7 +225,7 @@ namespace Skrivprogram___RichTextBox
             DialogResult dialogResult = MessageBox.Show("Vill du spara?", "Spara?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
             if (dialogResult == DialogResult.Yes)
             {
-                save();
+                //save();
                 richTextBox1.Clear();
                 sparat = false;
                 savePath = "";
@@ -241,7 +265,7 @@ namespace Skrivprogram___RichTextBox
 
         private void sparaToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            save();
+            //save();
         }
 
 
@@ -265,20 +289,6 @@ namespace Skrivprogram___RichTextBox
 
 
         }
-
-        private void läsFormattering()
-        {
-            int lineCounter = 0;
-            foreach (var line in richTextBox1.Lines)
-            {
-
-                richTextBox1.Select(richTextBox1.GetFirstCharIndexFromLine(lineCounter), line.Length);
-                label1.Text = label1.Text + richTextBox1.SelectionFont;
-                richTextBox1.Select();
-                lineCounter++;
-            }
-        }
-
         private int ordräknare()
         {
             int count = 0;
@@ -318,5 +328,89 @@ namespace Skrivprogram___RichTextBox
             int plats = richTextBox1.SelectionStart - richTextBox1.GetFirstCharIndexOfCurrentLine();
             lblPlats.Text = plats.ToString();
         }
+
+        private string läsFormattering()
+        {
+            string text = richTextBox1.Text;
+            string textExtra = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                // Extract the character at the current index
+                char currentChar = text[i];
+
+                // Get the character's formatting
+                int charFormatStart = richTextBox1.GetFirstCharIndexFromLine(richTextBox1.GetLineFromCharIndex(i));
+                int charFormatLength = i - charFormatStart + 1;
+                //string charFormat = richTextBox1.Text.Substring(charFormatStart, charFormatLength);
+
+                // Get the font style of the character
+                richTextBox1.SelectionStart = i;
+                Font charFormatFont = richTextBox1.SelectionFont;
+                Color charColor = richTextBox1.SelectionColor;
+
+                // Print out the character, its formatting and font style
+                //rtbDebug.AppendText("Character: " + currentChar + ", Font: " + charFormatFont + ", Color: " + charColor);
+                textExtra = textExtra + "{" + currentChar + "Î" + charFormatFont + "Î" + charColor + "}";
+            }
+            return textExtra;
+        }
+
+        
+
+        private void decodeFormatting(string formattedText)
+        {
+            List<string> tecken = formattedText.Split('}').ToList();
+            tecken.RemoveAt(tecken.Count - 1);
+            int index = 0;
+            foreach(string teckenItem in tecken)
+            {
+                //rtbDebug.AppendText(teckenItem + "\n");
+                string text = teckenItem.Remove(0, 1);
+                List<string> format = text.Split('Î').ToList();
+                richTextBox1.AppendText(format[0]);
+                richTextBox1.Select(index, 1);
+                richTextBox1.SelectionFont = decodeFont(format[1]);
+                
+                //richTextBox1.SelectionFont = format[1];
+
+                index++;
+            }
+        }
+
+        private Font decodeFont(string fontString)
+        {
+            //string fontString = "[Font: Name=Microsoft Sans Serif, Size=9, Units=3, GdiCharSet=0, GdiVerticalFont=False]";
+
+            // Remove the "[Font: " and "]" from the string
+            fontString = fontString.Remove(0, 7);
+            fontString = fontString.Remove(fontString.Length - 1);
+
+            // Split the string into individual properties
+            string[] properties = fontString.Split(',');
+
+            // Create a new Font object with the default font style
+            Font font = new Font(properties[0].Split('=')[1], 9);
+
+            // Apply the remaining properties to the font
+            for (int i = 1; i < properties.Length; i++)
+            {
+                string[] property = properties[i].Split('=');
+                string propertyName = property[0].Trim();
+                string propertyValue = property[1].Trim();
+
+                if (propertyName.Contains("Size"))
+                {
+                    //MessageBox.Show("Size" + propertyValue);
+                    float size = float.Parse(propertyValue);
+                    font = new Font(font.Name, size);
+                }
+
+                rtbDebug.AppendText(properties[i].ToString() + "\n");
+            }
+            //rtbDebug.AppendText(font.ToString() + "\n");
+            return font;
+
+        }
+
     }
 }
