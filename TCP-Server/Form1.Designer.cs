a1079f7ea5019cb76ad3541d3f9c55c0984befd3
Emil Zackrisson <emil.zackrisson@gmail.com>
2023-03-07 10:41:30 +0100

    boll

--- TCP-Server/Form1.Designer.cs
+++ TCP-Server/Form1.Designer.cs
@@ -28,9 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.rtbInkorg = new System.Windows.Forms.RichTextBox();
             this.btnStartServer = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnLeft = new System.Windows.Forms.Panel();
+            this.pnRight = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnBall = new System.Windows.Forms.Panel();
+            this.pnTop = new System.Windows.Forms.Panel();
+            this.pnBottom = new System.Windows.Forms.Panel();
+            this.lblLeftPoints = new System.Windows.Forms.Label();
+            this.lblRightPoints = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbInkorg
@@ -39,7 +50,7 @@
             this.rtbInkorg.Location = new System.Drawing.Point(12, 57);
             this.rtbInkorg.Name = "rtbInkorg";
             this.rtbInkorg.ReadOnly = true;
-            this.rtbInkorg.Size = new System.Drawing.Size(568, 502);
+            this.rtbInkorg.Size = new System.Drawing.Size(568, 489);
             this.rtbInkorg.TabIndex = 0;
             this.rtbInkorg.Text = "";
             // 
@@ -62,17 +73,98 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Inkorg";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.pnBottom);
+            this.panel1.Controls.Add(this.pnTop);
+            this.panel1.Controls.Add(this.pnBall);
+            this.panel1.Controls.Add(this.pnRight);
+            this.panel1.Controls.Add(this.pnLeft);
+            this.panel1.Location = new System.Drawing.Point(586, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(815, 489);
+            this.panel1.TabIndex = 3;
+            // 
+            // pnLeft
+            // 
+            this.pnLeft.BackColor = System.Drawing.Color.White;
+            this.pnLeft.Location = new System.Drawing.Point(3, 194);
+            this.pnLeft.Name = "pnLeft";
+            this.pnLeft.Size = new System.Drawing.Size(6, 50);
+            this.pnLeft.TabIndex = 0;
+            // 
+            // pnRight
+            // 
+            this.pnRight.BackColor = System.Drawing.Color.White;
+            this.pnRight.Location = new System.Drawing.Point(806, 194);
+            this.pnRight.Name = "pnRight";
+            this.pnRight.Size = new System.Drawing.Size(6, 50);
+            this.pnRight.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnBall
+            // 
+            this.pnBall.BackColor = System.Drawing.Color.White;
+            this.pnBall.Location = new System.Drawing.Point(382, 234);
+            this.pnBall.Name = "pnBall";
+            this.pnBall.Size = new System.Drawing.Size(10, 10);
+            this.pnBall.TabIndex = 2;
+            // 
+            // pnTop
+            // 
+            this.pnTop.Location = new System.Drawing.Point(0, -6);
+            this.pnTop.Name = "pnTop";
+            this.pnTop.Size = new System.Drawing.Size(815, 10);
+            this.pnTop.TabIndex = 3;
+            // 
+            // pnBottom
+            // 
+            this.pnBottom.Location = new System.Drawing.Point(-1, 484);
+            this.pnBottom.Name = "pnBottom";
+            this.pnBottom.Size = new System.Drawing.Size(815, 10);
+            this.pnBottom.TabIndex = 4;
+            // 
+            // lblLeftPoints
+            // 
+            this.lblLeftPoints.AutoSize = true;
+            this.lblLeftPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLeftPoints.Location = new System.Drawing.Point(583, 17);
+            this.lblLeftPoints.Name = "lblLeftPoints";
+            this.lblLeftPoints.Size = new System.Drawing.Size(30, 31);
+            this.lblLeftPoints.TabIndex = 4;
+            this.lblLeftPoints.Text = "0";
+            // 
+            // lblRightPoints
+            // 
+            this.lblRightPoints.AutoSize = true;
+            this.lblRightPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRightPoints.Location = new System.Drawing.Point(1371, 20);
+            this.lblRightPoints.Name = "lblRightPoints";
+            this.lblRightPoints.Size = new System.Drawing.Size(30, 31);
+            this.lblRightPoints.TabIndex = 5;
+            this.lblRightPoints.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 571);
+            this.ClientSize = new System.Drawing.Size(1564, 557);
+            this.Controls.Add(this.lblRightPoints);
+            this.Controls.Add(this.lblLeftPoints);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnStartServer);
             this.Controls.Add(this.rtbInkorg);
             this.Name = "Form1";
             this.Text = "TCP-Server";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,6 +175,15 @@
         private System.Windows.Forms.RichTextBox rtbInkorg;
         private System.Windows.Forms.Button btnStartServer;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnRight;
+        private System.Windows.Forms.Panel pnLeft;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel pnBall;
+        private System.Windows.Forms.Panel pnBottom;
+        private System.Windows.Forms.Panel pnTop;
+        private System.Windows.Forms.Label lblLeftPoints;
+        private System.Windows.Forms.Label lblRightPoints;
     }
 }
 
--- TCP-Server/Form1.cs
+++ TCP-Server/Form1.cs
@@ -18,6 +18,16 @@ namespace TCP_Server
         TcpClient klient;
         int port = 34512;
 
+        int vX = 1;
+        int vY = 1;
+
+        Point ballStart;
+        Point playerRightStart;
+        Point playerLeftStart;
+
+        Player playerRight;
+        Player playerLeft;
+
         public Form1()
         {
             InitializeComponent();
@@ -31,8 +41,6 @@ namespace TCP_Server
             btnStartServer.Enabled = false;
             StartaMottagning();
         }
-
-
 
         public async void StartaMottagning()
         {
@@ -55,9 +63,16 @@ namespace TCP_Server
             try
             {
                 n = await klient.GetStream().ReadAsync(buffert, 0, buffert.Length);
+                string text = Encoding.UTF8.GetString(buffert, 0, n);
 
-                byte[] utData = Encoding.UTF8.GetBytes("hejsan från servern");
-                await klient.GetStream().WriteAsync(utData, 0, utData.Length);
+                if (text.Contains("Left"))
+                {
+                    playerLeft.SetPlayer(text);
+                }
+                if (text.Contains("Right"))
+                {
+                    playerRight.SetPlayer(text);
+                }
             }
             catch (Exception error)
             {
@@ -67,10 +82,81 @@ namespace TCP_Server
             StartaLäsning(klient);
         }
 
+        public async void send(string message, TcpClient klient)
+        {
+            byte[] utData = Encoding.UTF8.GetBytes(message);
+            await klient.GetStream().WriteAsync(utData, 0, utData.Length);
+        }
+
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             klient.Close();
             klient.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playerRight = new Player(pnRight.Location, "Right");
+            playerLeft = new Player(pnLeft.Location, "Left");
+            timer1.Start();
+
+            playerRightStart = playerRight.Location;
+            playerLeftStart = playerLeft.Location;
+            ballStart = pnBall.Location;
+
+        }
+
+        private void resetPositions()
+        {
+            pnBall.Location = ballStart;
+            playerLeft.Location = playerLeftStart;
+            playerRight.Location = playerRightStart;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point ball = pnBall.Location;
+            ball.X += vX;
+            ball.Y += vY;
+            pnBall.Location = ball;
+
+            if (pnBall.Bounds.IntersectsWith(pnRight.Bounds))
+            {
+                vX = -1;
+            }
+            if (pnBall.Bounds.IntersectsWith(pnLeft.Bounds))
+            {
+                vX = 1;
+            }
+            if (pnBall.Bounds.IntersectsWith(pnTop.Bounds)) // Slår i taket
+            {
+                vY = 1;
+            }
+            if (pnBall.Bounds.IntersectsWith(pnBottom.Bounds)) // Slår i golvet
+            {
+                vY = -1;
+            }
+
+            // Kolla om boll utanför
+            if (ball.X < 0)
+            {
+                playerRight.AddPoint();
+                lblRightPoints.Text = playerRight.Point.ToString();
+                resetPositions();
+            }
+            if (ball.X > 816)
+            {
+                playerLeft.AddPoint();
+                lblLeftPoints.Text = playerLeft.Point.ToString();
+                resetPositions();
+
+
+            }
+
+            //send(ball.ToString());
+            //send(playerLeft.ToString());
+            //send(playerRight.ToString());
         }
     }
 }
